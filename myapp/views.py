d5e5f3d2dded98ea60e491236d3eee5511158281
leanne001 <leanne001@naver.com>
2019-06-08 18:29:51 +0900

    first commit

--- myapp/views.py
+++ myapp/views.py
@@ -10,20 +10,16 @@ def about(request):
     return render(request, 'about.html')
 
 def result(request):
-    full_text = request.GET['fulltext']
+    text = request.GET['fulltext']
 
-    word_list = full_text.split()
-    #공백 기준 나누기
+    word_list = text.split()
+
     word_dictionary = {}
-    #빈 사전형 객체
 
     for word in word_list:
         if word in word_dictionary:
-            # 한번 등장했던(사전 안에 있는) 단어 
             word_dictionary[word] += 1
         else:
-            # 새로운 (사전 안에 없는) 단어
             word_dictionary[word] = 1
-            # 해당하는 word를 key값 삼아서 value를 1로 해라.
 
-    return render(request, 'result.html', {'fulltext': full_text, 'total': len(word_list)})
+    return render(request, 'result.html', {'full': text, 'total': len(word_list), 'dictinary': word_dictionary.items()})
